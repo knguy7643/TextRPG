2f3caa575032e478902e52db40f27fd2996bf4d7
knguy7643 <knguyen7643@gmail.com>
2020-07-20 19:45:47 -0500

    Figuring out how stats will work.

--- src/Class.java
+++ src/Class.java
@@ -1,6 +1,43 @@
 
 public enum Class {
 
-	Fighter, Braver, Swordsman, Force, Bouncer;
+	Fighter, Braver, Swordsman, Force, Assassin;
+	
+	/*
+	 * Force - Starts all fights with an extra mana.
+	 * Fighter - Higher HP and Attack.
+	 * Braver - Highest Attack but lower Defense.
+	 * Swordsman - Higher HP and Defense.
+	 * Assassin - Higher Atk and Agility.
+	 */
+	
+	public int[] getStats() {
+		
+		if (this == Class.Fighter) {
+			int[] arr= {0, 0 , 0, 0};
+			return arr;
+		}
+		else if (this == Class.Braver) {
+			int[] arr= {0, 0 , 0, 0};
+			return arr;
+		}
+		else if (this == Class.Swordsman) {
+			int[] arr= {0, 0 , 0, 0};
+			return arr;
+		}
+		else if (this == Class.Force) {
+			int[] arr= {0, 0 , 0, 0};
+			return arr;
+		}
+		else {
+			int[] arr= {0, 0 , 0, 0};
+			return arr;
+		}
+		
+	}
+	
+	public int[] levelUpBonus() {
+		return null;
+	}
 	
 }
--- src/Player.java
+++ src/Player.java
@@ -4,6 +4,46 @@ public class Player {
 	private Class playerClass;
 	private Ability[] abilities = new Ability[4];
 	private String playerName;
+	private int totalExp;
 	
+	private final int MAX_MANA = 4;
+	
+	//Stats
+	private int currentHP;
+	private int maxHP;
+	private int manaPool;
+	private int atk;
+	private int def;
+	private int agility;
+	
+	public Player(String name, Class playerClass) {
+		playerName = name;
+		
+		this.playerClass = playerClass;
+		
+		totalExp = 0;
+		
+		setMana();
+		
+		
+		
+		currentHP = maxHP;
+	}
+	
+	public void reset() {
+		currentHP = maxHP;
+		
+		setMana();
+		
+	}
+	
+	public void setMana() {
+		if (this.playerClass == Class.Force) {
+			manaPool = 1;
+		}
+		else {
+			manaPool = 0;
+		}
+	}
 	
 }
